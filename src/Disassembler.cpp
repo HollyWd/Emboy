a217768bca4affb8877e4ab0e02cef0c17534bf9
M Kel <16032122+Okaerikoto@users.noreply.github.com>
2019-03-19 16:22:21 +0100

    Fix disassemble from string

--- src/Disassembler.cpp
+++ src/Disassembler.cpp
@@ -3,24 +3,42 @@
 
 void Disassembler::disassemble_instruction(const std::string &instruction_string) const
 {
-	std::string hexref("0123456789abcdef");
+  std::vector<char> byte_vect;
+//Convert string to a vector of byte
+  std::string sconvert("0123456789ABCDEF");
+ 
+  std::istringstream iss(instruction_string);
 
-	std::stringstream s_stream(instruction_string);
-	std::vector<char> char_vect;
-	int number_b10;
-	while(s_stream>>number_b10){
-		std::cout <<"number_b10 " <<(int)number_b10<<std::endl;
-		int number_b16 = 16 * (number_b10/10) + number_b10%10;
-		char_vect.push_back(number_b16);
-		std::cout <<"number_b16 " <<(int)number_b16<<std::endl;
-	}
-	for(auto c : char_vect){
-		std::cout << std::hex <<(int)c<<" "<<std::endl;
-	}
-	//TODO : not working for letters...
+    while(iss){
+        std::string op_byte("");
+        iss >> op_byte; // get the next byte from the stream
+        
+        if(op_byte==""){
+            break; //end of string
+        }
+        
+        std::cout<<"code read "<<op_byte<<std::endl;
 
+        if (op_byte.size()!=2){
+          std::cerr<<"Error : the input code is expected to be 2 digits hexadecimal byte separated by spaces "<<std::endl;
+          return 1;
+        }
+
+        int hb = sconvert.find(op_byte[0]);
+        int lb = sconvert.find(op_byte[1]);
+
+        int val = hb*16+lb;
+        byte_vect.push_back(val);
+        std::cout<<"value "<<val<<std::endl;        
+    }
+	//TODO disassemble(byte_vector)
+    return 0;
 }
-
-
+    //// to test do
+    //std::string s;
+    //std::getline(std::cin,s);
+    //disassemble_instruction(s);
+    //
+    //return 0;
 
 
